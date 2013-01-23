b757e3ff9f332b46b40e3558101c602dc96f8f87
root <root@ubuntu.ubuntu-domain>
2013-01-23 23:55:47 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mplyr::Application.config.secret_token = '755463e7fb0bab487ea58beb0447ddf68f4dcab1530ddda3b4e9b4b47ebdca160eb107bb51fce764e70d9c1f044160b4fb60a0ed270cdf0d8d6aabc9898b34f6'
